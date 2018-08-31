285f4a6b84b88d58976eeb9343e878df63317c60
Mai Kalange <joseph.nyirenda@gmail.com>
2018-08-31 11:12:54 +0100

    Added Solution to Quadratic Equations

---
+++ Selections/Selections/QuadraticEquation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selections
+{
+    class QuadraticEquation
+    {
+        public static void PromptForConstants()
+        {
+            Console.WriteLine("Enter the value for a, b and C");
+            String constants  = Console.ReadLine();
+            String[] values  = constants.Split(new char []{ ' '});
+            if (values.Length == 3)
+            {
+                var a = values[0];
+                var b = values[1];
+                var c = values[2];
+            }
+
+        }
+
+
+
+        private static bool IsRealRoots()
+        {
+            return false;
+        }
+    }
+}
