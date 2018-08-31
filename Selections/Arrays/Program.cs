92168d393a480599c30a3815b9143c00240142ba
Mai Kalange <joseph.nyirenda@gmail.com>
2018-09-01 00:56:32 +0100

    Arrays

--- Selections/Arrays/Program.cs
+++ Selections/Arrays/Program.cs
@@ -12,6 +12,41 @@ namespace CSharpFundamentals
         {
             static void Main(string[] args)
             {
+                int[] r = {1, 9, 5, 3, 2, 2, 1, 8, 1, 5, 2, 3};
+                
+                int[] u = {1, 9, 5, 3, 2, 8};
+                
+                EliminateDuplicates(r);
+            }
+
+            private static void  EliminateDuplicates(int[] list)
+            {
+                //copy elements to a string array
+                string[] p = new string[list.Length];
+                for (int i = 0; i < list.Length; i++)
+                {
+                    p[i] = list[i].ToString();
+                }
+                //check if the array has duplicates
+                var elements = new StringBuilder();
+                for (int i = 0; i < list.Length; i++)
+                {
+                    for (int j=i+1;j < list.Length;j++)
+                    {
+                        if (list[i] == list[j])
+                        {
+                            p[j] = string.Empty;
+                        }
+                    }
+                }
+
+                foreach (var i in p)
+                {
+                    if (i!=string.Empty)
+                    {
+                        Console.Write(i +"  ");
+                    } 
+                }
             }
         }
     }
