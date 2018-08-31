b3d685e8e83648f4707322b894d465541b290933
Mai Kalange <joseph.nyirenda@gmail.com>
2018-08-31 11:02:08 +0100

    New methods for selections

--- Selections/Selections/Program.cs
+++ Selections/Selections/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Selections
 {
@@ -10,6 +6,50 @@ namespace Selections
     {
         static void Main(string[] args)
         {
+        }
+
+        private static void Example2(int x)
+        {
+            switch (Math.Abs(x%2))
+            {
+                case 0: Console.WriteLine("Even number");break;
+                case 1: Console.WriteLine("Odd number"); break;
+                default: Console.WriteLine("Number unknown");break;
+            }
+        }
+
+        private static void Example3(int NumberOfTosses)
+        {
+            const string HEADS = "HEADS";
+            const string TAILS = "TAILS";
+            Random r = new Random();
+            //HEADS =1, TAILS =0
+      
+            for (int i = 0; i < NumberOfTosses; i++)
+            {
+                if (Math.Floor(r.Next() * 2d) == 1)
+                {
+                    Console.WriteLine(HEADS);
+                }
+                else
+                {
+                    Console.WriteLine(TAILS);
+                }
+            }
+            
+        }
+
+        private static void Example1(int x)
+        {
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+
         }
     }
 }
